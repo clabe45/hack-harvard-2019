5fa8dd0368e8fa28d7311af8199ecf4809a17d4b
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-10-19 17:27:00 -0400

    Shrink labels

--- src/client/components/Interest.js
+++ src/client/components/Interest.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Label } from 'semantic-ui-react';
 
 export default class Interest extends React.Component {
+  getStyle() {
+    return {
+      fontSize: '10px'
+    }
+  }
+
   render() {
     return (
       <a>
-        <Label className='blue'>{this.props.name}</Label>
+        <Label className='blue' style={this.getStyle()}>{this.props.name}</Label>
       </a>
     );
   }
--- src/client/components/Skill.js
+++ src/client/components/Skill.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Label } from 'semantic-ui-react';
 
 export default class Skill extends React.Component {
+  getStyle() {
+    return {
+      fontSize: '10px'
+    }
+  }
+
   render() {
     return (
       <a>
-        <Label className='red'>{this.props.name}</Label>
+        <Label className='red' style={this.getStyle()}>{this.props.name}</Label>
       </a>
     );
   }
