cddf61952835313c7d1de0f2ffb7576e90cd85ce
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-10-19 21:59:20 -0400

    Improve navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Segment, Image, Grid } from 'semantic-ui-react';
+
 // import logo from './logo.svg';
 import Home from './client/pages/home/Home';
 import './App.css';
-import Header from './client/header/Header';
 // import uuid from 'uuid';
 import User from './client/pages/user/User';
 // import Projects from './client/pages/Projects';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import favicon from './client/assets/favicon.png';
 
 class App extends Component {
   render() {
+    const liStyle = {
+      padding: 10
+    };
+
     return (
-        <Router>
-          <div className="App">
-          <Header />
-          <Route exact path="/" component={Home}/>
-          <Route path="/user" component={User} />
-        </div>
+      <Router>
+        <div className="App">
+          { /* Top image */ }
+          <div className="top ui" textAlign="center">
+            <a href="/"><Image style={{margin: 'auto'}} size="tiny" src={favicon}></Image></a>
+          </div>
+          <div className="bottom">
+            <Grid>
+              <Grid.Column width="2" className="sidebar" style={{marginTop: 20}}>
+                <ul style={{listStyle: 'none'}}>
+                  <li style={liStyle}><Link to='/'><h3>Home</h3></Link></li>
+                  <li style={liStyle}><Link to='/messages'><h3>Messages</h3></Link></li>
+                  <li style={liStyle}><Link to='/projects'><h3>Projects</h3></Link></li>
+                </ul>
+              </Grid.Column>
+              <Grid.Column width="14" className="content">
+                <Route exact path="/" component={Home}/>
+                <Route path="/user" component={User} />
+              </Grid.Column>
+            </Grid>
+          </div>
+          </div>
       </Router>
     );
   }
