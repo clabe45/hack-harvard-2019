f366b5d26ea3acfa49132288679d7896f129b121
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-10-20 05:53:14 -0400

    Restart home page

--- src/client/pages/home/Home.js
+++ src/client/pages/home/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Message, Table, Image } from 'semantic-ui-react';
+
+import logo from './assets/logo.svg';
 
 export default class Home extends React.Component {
   render() {
-    return 'sample text';
+    return (
+      <div>
+        <Message color="green" style={{ borderRadius: 0 }}>
+          Thanks for signing up to DevNet! Start your network by finding other devs or finding projects to work on.
+        </Message>
+      </div>
+    );
   }
 }
